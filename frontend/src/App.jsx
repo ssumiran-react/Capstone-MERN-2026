eb30fe984080be2c48d8454e937fd83bdd9bdd50
ssumiran <ssumiran@yahoo.com>
2026-08-04 09:40:33 -0400

    Create BrowserRouter Routes features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Login from './pages/Login';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/Login" element={<Login/>}/>
+        <Route path="" element={<Login/>}/>
+      </Routes>
     </>
+
+    
   )
 }
 
